7ff556226cfa7918f5cf3da435421a08e36fc34c
ancaspn <88373468+ancaspn@users.noreply.github.com>
2021-08-08 13:05:43 +0700

    Tugas Seleksi Nilai dalam Materi Unit Test

---
+++ services/siswa.go
@@ -0,0 +1,15 @@
+package siswa
+
+// jika nilai <= 40, maka nilai C
+// jika nilai > 40 dan <= 70, maka nilai B
+// jika nilai > 70, maka nilai A
+
+func grade(nilai int) string {
+	if nilai <= 40 {
+		return "C"
+	} else if nilai > 40 && nilai <= 70 {
+		return "B"
+	} else {
+		return "A"
+	}
+}
---
+++ services/siswa_test.go
@@ -0,0 +1,43 @@
+package siswa
+
+import "testing"
+
+func Test_grade(t *testing.T) {
+	type args struct {
+		nilai int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "case return C",
+			args: args{
+				nilai: 30,
+			},
+			want: "C",
+		},
+		{
+			name: "case return B",
+			args: args{
+				nilai: 60,
+			},
+			want: "D",
+		},
+		{
+			name: "case return A",
+			args: args{
+				nilai: 80,
+			},
+			want: "A",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := grade(tt.args.nilai); got != tt.want {
+				t.Errorf("grade() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
